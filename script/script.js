aceb8a9e400c3e4d20b8e0fb7066bc518a11c098
Armaankhaan01 <armankhan876426@gmail.com>
2022-12-20 18:58:06 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,66 @@
+var screen = document.querySelector('#screen');
+var buttons = document.querySelectorAll('.button');
+var buttons = document.querySelectorAll('.button');
+
+
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        btntext = e.target.innerText;
+        if (btntext == 'X') {
+            btntext = '*';
+        }
+        if (btntext == '÷') {
+            btntext = '/';
+        }
+        // if (btntext == '=') {
+        //     demo.innerHTML += `Clicked ${btntext}`;
+        //     setTimeout(() => {
+        //         demo.innerHTML = '';
+        //     }, 3000);
+
+        // }
+        screen.value += btntext;
+    })
+
+    function sin() {
+        screen.value = Math.sin(screen.value);
+    }
+    function cos() {
+        screen.value = Math.cos(screen.value);
+    }
+    function tan() {
+        screen.value = Math.tan(screen.value);
+    }
+    function pow() {
+        screen.value = Math.pow(screen.value);
+    }
+    function sqrt() {
+        screen.value = Math.sqrt(screen.value);
+    }
+    function log() {
+        screen.value = Math.log(screen.value);
+    }
+
+    function pi() {
+        screen.value = 3.14159265359;
+    }
+
+    function e() {
+        screen.value = 2.71828182846;
+    }
+
+    function fact() {
+        let i, num, f;
+        f = 1;
+        num = screen.value;
+        for (i = 1; i <= num; i++) {
+            f = f * i;
+        }
+        i = i - 1;
+        screen.value = f;
+    }
+    function backspace() {
+        screen.value = screen.value.substr(0, screen.value.length - 1)
+    }
+
+}
